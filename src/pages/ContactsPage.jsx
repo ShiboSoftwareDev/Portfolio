f0150191d9cfc5008a0358ff708b2fe2ffd5a894
Ahmed <ahmedsheb2003@yahoo.com>
2024-01-20 21:07:43 +0200

    cleaned up the flip card

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 export default function ContactsPage() {
-  //Im still copying styles from index.css to tailwind styles
-
-  //and copying an idea from a favorited site
+  //Animate some shapes that translate or pop into the card when hovered over
 
   const [flipped, setFlipped] = React.useState(false);
   function flip() {
@@ -16,16 +14,20 @@ export default function ContactsPage() {
       className="bg-gray-800 text-center w-full h-[900px] flex flex-col items-center"
     >
       <div
+        id="flip-card-container"
         onClick={flip}
-        className=" bg-transparent w-[500px] h-[700px] [perspective:2000px] z-10"
+        className="bg-transparent w-[500px] h-[700px] [perspective:2000px] z-10"
       >
         <div
-          className={`flip-card-inner relative w-[100%] h-[100%] text-center [transition:transform_0.8s] [transform-style:preserve-3d] ${
+          id="flip-card-inner"
+          className={`relative w-[100%] h-[100%] text-center [transition:transform_0.8s] [transform-style:preserve-3d] ${
             flipped ? "[transform:rotateY(180deg)]" : ""
           }`}
         >
-          {/* bg-slate-600 */}
-          <div className="flip-card-front text-blue-500 border-blue-500 border-2 [-webkit-backface-visibility:hidden] [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-violet-700 bg-opacity-5 absolute ">
+          <div
+            id="flip-card-front"
+            className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-violet-700 bg-opacity-5 absolute overflow-hidden rounded-lg"
+          >
             <h1 className="z-10">Contacts Page 1</h1>
             <p>Non consectetur a erat nam at.</p>
             <p>Non consectetur a erat nam at.</p>
@@ -38,9 +40,10 @@ export default function ContactsPage() {
             <p>Non consectetur a erat nam at.</p>
             <p>Non consectetur a erat nam at.</p>
           </div>
-          {/* bg-slate-600 */}
-          {/*  */}
-          <div className="flip-card-back text-blue-500 border-blue-500 border-2 [-webkit-backface-visibility:hidden] [backface-visibility:hidden] [backdrop-filter:blur(5px)] [transform:rotateY(180deg)] w-[100%] h-[100%] bg-violet-700 bg-opacity-5">
+          <div
+            id="flip-card-back"
+            className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-violet-700 bg-opacity-5 absolute overflow-hidden rounded-lg [transform:rotateY(180deg)]"
+          >
             <h1 className="z-10">Contacts Page 2</h1>
             <p>Non consectetur a erat nam at.</p>
             <p>Non consectetur a erat nam at.</p>
