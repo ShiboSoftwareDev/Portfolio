72ef3d2c52b09a2754f5d0ed8d45a882562a33eb
Ahmed <ahmedsheb2003@yahoo.com>
2024-01-21 22:26:28 +0200

    modified contact image colors

--- src/components/CardBack.jsx
+++ src/components/CardBack.jsx
@@ -7,7 +7,7 @@ export default function CardBack() {
     <div
       onMouseEnter={() => setStarState(true)}
       onMouseLeave={() => setStarState(false)}
-      className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-violet-700 bg-opacity-5 absolute overflow-hidden rounded-lg [transform:rotateY(180deg)]"
+      className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-purple-700 bg-opacity-5 absolute overflow-hidden rounded-lg [transform:rotateY(180deg)]"
     >
       {starState ? <StaticStars count={30} indexZ={-1} /> : null}
       <h1>Form</h1>
--- src/components/CardFront.jsx
+++ src/components/CardFront.jsx
@@ -7,13 +7,13 @@ export default function CardFront() {
     <div
       onMouseEnter={() => setStarState(true)}
       onMouseLeave={() => setStarState(false)}
-      className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-violet-700 bg-opacity-5 absolute overflow-hidden rounded-lg"
+      className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-purple-700 bg-opacity-5 absolute overflow-hidden rounded-lg"
     >
       {starState ? <StaticStars count={30} indexZ={-1} /> : null}
       <h1 className="absolute w-[100%]">You can fine me on:</h1>
       <div className="w-[100%] h-[100%] grid content-center">
         <a
-          className="w-[15%] mb-14 aspect-[1/1] bg-white bg-opacity-80 drop-shadow-[0_0_10px_rgba(126,34,206,1)] rounded-3xl justify-self-center"
+          className="w-[15%] mb-14 aspect-[1/1] bg-purple-700 bg-opacity-5 drop-shadow-[0_0_10px_rgba(126,34,206,1)] rounded-3xl justify-self-center"
           onClick={(event) => event.stopPropagation()}
           href="https://github.com/ShiboSoftwareDev"
           target="blank"
@@ -25,7 +25,7 @@ export default function CardFront() {
           ></img>
         </a>
         <a
-          className="w-[15%] mb-14 aspect-[1/1] bg-white bg-opacity-80 drop-shadow-[0_0_10px_rgba(126,34,206,1)] rounded-3xl justify-self-center"
+          className="w-[15%] mb-14 aspect-[1/1] bg-purple-700 bg-opacity-5 drop-shadow-[0_0_10px_rgba(126,34,206,1)] rounded-3xl justify-self-center"
           onClick={(event) => event.stopPropagation()}
           href="https://x.com/elonmusk?s=20"
           target="blank"
@@ -37,7 +37,7 @@ export default function CardFront() {
           ></img>
         </a>
         <a
-          className="w-[15%] mb-14 aspect-[1/1] bg-white bg-opacity-80 drop-shadow-[0_0_10px_rgba(126,34,206,1)] rounded-3xl justify-self-center"
+          className="w-[15%] mb-14 aspect-[1/1] bg-purple-700 bg-opacity-5 drop-shadow-[0_0_10px_rgba(126,34,206,1)] rounded-3xl justify-self-center"
           onClick={(event) => event.stopPropagation()}
           href="https://www.linkedin.com/in/ahmed-shebani-3563142a6/"
           target="blank"
