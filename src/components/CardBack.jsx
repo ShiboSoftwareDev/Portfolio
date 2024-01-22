a0396c00d1535806933cfc7368f050f764de83ca
Ahmed <ahmedsheb2003@yahoo.com>
2024-01-22 22:25:01 +0200

    still working on second side of contact card

--- src/components/CardBack.jsx
+++ src/components/CardBack.jsx
@@ -10,12 +10,73 @@ export default function CardBack() {
       className="text-blue-500 border-blue-500 border-2 [backface-visibility:hidden] [backdrop-filter:blur(5px)] w-[100%] h-[100%] bg-purple-700 bg-opacity-5 absolute overflow-hidden rounded-lg [transform:rotateY(180deg)]"
     >
       {starState ? <StaticStars count={30} indexZ={-1} /> : null}
-      <h1>Form</h1>
-      <p>Type your name,</p>
-      <p>type your email,</p>
-      <p>type your message,</p>
-      <p>submit button,</p>
-      <p>Send message to my email.</p>
+      <div className="login-box absolute -translate-x-[50%] -translate-y-[50%] left-[50%] top-[50%] w-[70%]">
+        <h2 className="mb-8">Message me</h2>
+        <form>
+          <div className="user-box relative">
+            <input
+              id="email"
+              className="w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
+              onClick={(e) => e.stopPropagation()}
+              type="email"
+              autoComplete="off"
+              name=""
+            ></input>
+            <label
+              htmlFor="email"
+              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none"
+            >
+              Email
+            </label>
+          </div>
+          <div className="user-box relative">
+            <input
+              id="name"
+              className="w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
+              onClick={(e) => e.stopPropagation()}
+              type="text"
+              autoComplete="off"
+              name=""
+            ></input>
+            <label
+              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none"
+              htmlFor="name"
+            >
+              Name
+            </label>
+          </div>
+          <div className="user-box relative">
+            <input
+              id="message"
+              className="w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
+              onClick={(e) => e.stopPropagation()}
+              type="text"
+              autoComplete="off"
+              name=""
+            ></input>
+            <label
+              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none"
+              htmlFor="message"
+            >
+              Message
+            </label>
+          </div>
+          <a
+            className="hover:text-white"
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+            }}
+            href="#"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            send
+          </a>
+        </form>
+      </div>
     </div>
   );
 }
