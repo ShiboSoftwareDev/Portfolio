f169cb1cb754982308ff2cdb8359984a6d02a12e
Ahmed <ahmedsheb2003@yahoo.com>
2024-01-23 17:40:02 +0200

    added a template for the message form

--- src/components/CardBack.jsx
+++ src/components/CardBack.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import StaticStars from "../components/StaticStars";
 
+// need to change anchor tag box shadow color
+
 export default function CardBack() {
   const [starState, setStarState] = React.useState(false);
   return (
@@ -16,7 +18,7 @@ export default function CardBack() {
           <div className="user-box relative">
             <input
               id="email"
-              className="w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
+              className="peer w-full py-2 mb-8 border-b-2 outline-none bg-transparent autofill:duration-[5000s]"
               onClick={(e) => e.stopPropagation()}
               type="email"
               autoComplete="off"
@@ -24,7 +26,7 @@ export default function CardBack() {
             ></input>
             <label
               htmlFor="email"
-              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none"
+              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none peer-focus:text-purple-700 peer-focus:-top-[21px] peer-focus:left-0 peer-autofill:text-purple-700 peer-autofill:-top-[21px] peer-autofill:left-0"
             >
               Email
             </label>
@@ -32,14 +34,14 @@ export default function CardBack() {
           <div className="user-box relative">
             <input
               id="name"
-              className="w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
+              className="peer w-full py-2 mb-8 border-b-2 outline-none bg-transparent autofill:duration-[5000s]"
               onClick={(e) => e.stopPropagation()}
               type="text"
               autoComplete="off"
               name=""
             ></input>
             <label
-              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none"
+              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none peer-focus:text-purple-700 peer-focus:-top-[21px] peer-focus:left-0 peer-autofill:text-purple-700 peer-autofill:-top-[21px] peer-autofill:left-0"
               htmlFor="name"
             >
               Name
@@ -48,32 +50,33 @@ export default function CardBack() {
           <div className="user-box relative">
             <input
               id="message"
-              className="w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
+              className="peer w-full py-2 mb-8 border-b-2 outline-none bg-transparent"
               onClick={(e) => e.stopPropagation()}
               type="text"
               autoComplete="off"
               name=""
             ></input>
             <label
-              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none"
+              className="absolute top-0 left-0 py-[10px] duration-500 pointer-events-none peer-focus:text-purple-700 peer-focus:-top-[21px] peer-focus:left-0 peer-autofill:text-purple-700 peer-autofill:-top-[21px] peer-autofill:left-0"
               htmlFor="message"
             >
               Message
             </label>
           </div>
           <a
-            className="hover:text-white"
+            className="relative inline-block py-3 px-5 overflow-hidden duration-500 mt-10 tracking-[4px] drop-shadow-[0_0_50px_#3b82f6] hover:text-white hover:bg-blue-500 hover:rounded-[5px] hover:shadow-[0_0_25px_#3b82f6,0_0_5px_#3b82f6]"
             onClick={(e) => {
               e.stopPropagation();
               e.preventDefault();
             }}
             href="#"
           >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            send
+            {/* forgot about the gradient background */}
+            <span className="absolute block animate-slider1 top-0 left-[-100%] w-full h-[2px] bg-blueGradient"></span>
+            <span className="absolute block animate-slider2 right-0 top-[-100%] w-[2px] h-full bg-blueGradient"></span>
+            <span className="absolute block animate-slider3 bottom-0 right-[-100%] w-full h-[2px] bg-blueGradient"></span>
+            <span className="absolute block animate-slider4 left-0 bottom-[-100%] h-full w-[2px] bg-blueGradient"></span>
+            SEND
           </a>
         </form>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,62 @@ export default {
           "80%": { opacity: "0" },
           "100%": { opacity: "0.1" },
         },
+        slide1: {
+          "0%": {
+            left: "-100%",
+          },
+          "50%": {
+            left: "100%",
+          },
+          "100%": {
+            left: "100%",
+          },
+        },
+        slide2: {
+          "0%": {
+            top: "-100%",
+          },
+          "50%": {
+            top: "100%",
+          },
+          "100%": {
+            top: "100%",
+          },
+        },
+        slide3: {
+          "0%": {
+            right: "-100%",
+          },
+          "50%": {
+            right: "100%",
+          },
+          "100%": {
+            right: "100%",
+          },
+        },
+        slide4: {
+          "0%": {
+            bottom: "-100%",
+          },
+          "50%": {
+            bottom: "100%",
+          },
+          "100%": {
+            bottom: "100%",
+          },
+        },
       },
       animation: {
         customPulse: "customPulse 3s ease-out",
         customAction: "customAction 3.5s ease-out",
         spinSlow: "spin 10s linear infinite, starPulse 10s ease-out infinite",
+        slider1: "slide1 1s linear infinite",
+        slider2: "slide2 1s 0.25s linear infinite",
+        slider3: "slide3 1s 0.5s linear infinite",
+        slider4: "slide4 1s 0.75s linear infinite",
+      },
+      backgroundImage: {
+        blueGradient: "linear-gradient(360deg, transparent, #3b82f6)",
       },
     },
   },
